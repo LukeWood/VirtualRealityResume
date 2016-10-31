851f68d717fb820dfed2054814fb8414bfcf2198
Luke Wood <lswood@smu.edu>
2016-10-31 14:44:26 -0500

    aframe' git p'

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-Vue.component('project', {
-  props:['project'],
-  template: `
-  <a-box src="#background" onClick="location.href=this.href">
-  </a-box>
-  `
-})
-var v = new Vue({
-  el:"#project_layout",
-  data:{
-    projects:[]
-  }
-});
 
+var root = document.getElementById("project_layout");
+var assets = document.getElementById("assets");
 $.getJSON("projects.json",function(data){
-  v.projects = data;
+
+  for(var i = 0; i < data.length; i++){
+    var project = data[i];
+    var img = document.createElement("img");
+    img.src = project.img;
+    img.setAttribute("id",i+"")
+    assets.append(img);
+    var el = document.createElement("a-box");
+    el.setAttribute("src",project.img);
+    el.setAttribute("scale","1.3 1.3 1.3");
+    el.addEventListener("click", function(){location.href = project.href;});
+    root.append(el);
+  }
+
 });
