ae74c76411d117676f44436c01e99c3a59a061e8
Luke Wood <lswood@smu.edu>
2016-11-01 16:02:31 -0500

    rotation

--- js/index.js
+++ js/index.js
@@ -12,6 +12,15 @@ $.getJSON("projects.json",function(data){
     var el = document.createElement("a-box");
     el.setAttribute("src",project.img);
     el.setAttribute("scale","1.3 1.3 1.3");
+    //
+    // var evt = document.createElement("a-animation");
+    // evt.setAttribute("attribute","rotation");
+    // evt.setAttribute("to","360 360 360");
+    // evt.setAttribute("repeat","indefinite");
+    // evt.setAttribute("dur","1000");
+    // evt.setAttribute("fill","forwards");
+    //el.appendChild(evt);
+
     el.addEventListener("click", function(){location.href = project.href;});
     root.append(el);
   })();
