222e9f9732955f5c252d66fe87a53ffd118345a1
Luke Wood <lswood@smu.edu>
2016-11-01 16:19:37 -0500

    companies

--- js/index.js
+++ js/index.js
@@ -27,3 +27,24 @@ $.getJSON("projects.json",function(data){
   }
 
 });
+var root_company = document.getElementById("company_layout");
+$.getJSON("companies.json",function(data){
+
+  for(var i = 0; i < data.length; i++){
+    (function(){
+      var project = data[i];
+      var img = document.createElement("img");
+      img.src = project.img;
+      img.setAttribute("id",i+"")
+      assets.append(img);
+      var el = document.createElement("a-box");
+      el.setAttribute("src",project.img);
+      el.setAttribute("scale",".5 .5 .5");
+
+
+      el.addEventListener("click", function(){location.href = project.href;});
+      root_company.append(el);
+  })();
+  }
+
+});
